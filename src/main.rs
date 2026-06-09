80df3044bff5edddb95d46be0f5cc4463f7eb18b
abhinavAwasthi9189 <awasthiabhinav2006@gmail.com>
2026-06-09 23:44:35 +0530

    first few addition as i think of personalize the budgetting for what i need

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+enum Account{
+    Saving{
+        amount:i32,
+    },
+    Credit{
+        amount:i32,
+        interest_rate:f32,
+    },
+    Periodic{
+        amount:i32,
+        periodic_increment:i32,
+    },
+}
+
+enum Change{
+    Deposit(i32),
+    Withdraw(i32),
+}
+
 fn main() {
     println!("Hello, world!");
 }
